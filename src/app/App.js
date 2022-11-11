1d907bba006fc960617c66394d852554e540e296
Bob <bobdawaagh@gmail.com>
2022-11-11 18:16:53 +0300

    add code

---
+++ src/app/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./App.css"
+import { Route } from 'react-router-dom'
+import Card from "./components/card"
+import Edit from "./components/edit"
+
+function App() {
+  return (
+    <div className="App">
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3 shadow p-4'>
+                    <Route exact path="/" component={Card}/>
+                    <Route path="/edit" component={Edit}/>
+                </div>
+            </div>
+        </div>
+    </div>
+        )
+  }
+export default App;
